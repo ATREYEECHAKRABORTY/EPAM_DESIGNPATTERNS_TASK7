3bcf21bb52f7842baf75e94298a76b4c16edb446
Atsy21 <atreyee.1999.ac@gmail.com>
2020-03-09 23:44:58 +0530

    TASK 7

---
+++ BehavourialPatterns/src/main/java/com/EPAM_Design_Patterns/BehavourialPatterns/App.java
@@ -0,0 +1,13 @@
+package com.EPAM_Design_Patterns.BehavourialPatterns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
